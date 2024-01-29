8e84627042f2626af4011adb1c3605a54be55c09
Priyanshu Ganatra <priyanshuganatra@gmail.com>
2024-01-29 16:07:02 +0530

    made some changes for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -33,18 +33,18 @@ import { githubInfoLoader } from './components/Github/getGithubInfo'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<Layout/>}>
-      <Route path='' element={<Home/>}/>
-      <Route path='about' element={<About/>}/>
-      <Route path='contact' element={<Contact/>}/>
-      <Route path='user/:userid' element={<User/>}/>
-      <Route path='github' loader={githubInfoLoader} element={<Github/>}/>
+    <Route path='/React-Router/' element={<Layout />}>
+      <Route path='' element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+      <Route path='user/:userid' element={<User />} />
+      <Route path='github' loader={githubInfoLoader} element={<Github />} />
     </Route>
   )
-) 
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
